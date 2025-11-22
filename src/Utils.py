f423113a1857cee674e7f62a88182e15f73df76a
AmreetNanda <amreetnanda321@gmail.com>
2025-11-22 19:27:39 +0530

    Model trainer is complete and Utils.py is adjusted

--- src/Utils.py
+++ src/Utils.py
@@ -1,65 +1,57 @@
 import os
 import sys
-
-import numpy as np
-import pandas as pd
-# import dill
 import pickle
-from tqdm import tqdm
-
+import numpy as np 
+import pandas as pd
+from src.Exception import CustomException
+from src.Logger import logging
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+from tqdm import tqdm
 
-from src.Exception import CustomException
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
 
-
-# Save the model
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
-# Load the model 
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
-    
-    except Exception as e:
-        raise CustomException (e,sys)
-
-#Evaluate the model
-def evaluate_model(X_train, y_train, X_test, y_test, models, param):
+def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
-
-        for i in tqdm(range(len(list(models)))):
+        for i in tqdm(range(len(models))):
             model = list(models.values())[i]
-
-            para = param[list(models.keys())[i]]
-
-            gs = GridSearchCV(model, para, cv = 5)
-            gs.fit(X_train, y_train)
-
-            model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)
-
+            # Train model
+            model.fit(X_train,y_train)
+            
+            # Predict the Training and Testing data
             y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            y_test_pred =model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            # Get R2 scores for train and test data
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
-        
+            report[list(models.keys())[i]] =  test_model_score
+
         return report
     
     except Exception as e:
-        raise CustomException(e,sys)
+            logging.info('Exception occured during model training')
+            raise CustomException(e,sys)
+    
 
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
